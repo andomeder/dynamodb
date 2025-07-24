7ca7fec0bca1d1b360b431a20b00d12622f46620
William Obino <obinowilliam@gmail.com>
2025-07-25 01:32:48 +0300

    feat: Implement dark mode toggle and dynamic theming
    Commit generated by VibeGit

---
+++ components/ToggleDark.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { useDark, useToggle } from '@vueuse/core'
+
+const isDark = useDark()
+const toggleDark = useToggle(isDark)
+</script>
+
+<template>
+  <button
+    class="bg-transparent !border-none !outline-none"
+    @click="toggleDark()"
+  >
+    <div v-if="isDark" class="i-carbon-sun" />
+    <div v-else class="i-carbon-moon" />
+  </button>
+</template>
